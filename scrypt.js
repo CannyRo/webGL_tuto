52e0f19ad94a6d7d8bc4189977a4405138ff8e16
CannyRo <69788762+CannyRo@users.noreply.github.com>
2024-04-17 12:35:28 +0200

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,25 @@
+console.log("Hello WebGL");
+//
+// Début ici
+//
+function main() {
+    const canvas = document.querySelector("#glCanvas");
+    // Initialisation du contexte WebGL
+    const gl = canvas.getContext("webgl");
+    // Continuer seulement si WebGL est disponible et fonctionnel
+    if (!gl) {
+        alert(
+        "Impossible d'initialiser WebGL. Votre navigateur ou votre machine peut ne pas le supporter.",
+        );
+        return;
+    }
+
+    // Définir la couleur d'effacement comme étant le noir, complètement opaque = le fond
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Effacer le tampon de couleur avec la couleur d'effacement spécifiée
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+//
+// Appel de la fonction Main
+//
+main();
